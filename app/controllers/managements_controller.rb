d803b789f4253616d69e79f1f80e32059977278a
yuchel-246 <yuya.f1117@icloud.com>
2020-12-06 14:12:40 +0900

    uikit、path、view

---
+++ app/controllers/managements_controller.rb
@@ -0,0 +1,8 @@
+class ManagementsController < ApplicationController
+  def result
+  end
+  def schedule
+  end
+  def ranking
+  end
+end
---
+++ test/controllers/managements_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ManagementsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
