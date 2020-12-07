5ca12735789a731c97a46481de5eee7cdfe43cc7
yuchel-246 <yuya.f1117@icloud.com>
2020-12-08 00:36:56 +0900

    group_number追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_155411) do
+ActiveRecord::Schema.define(version: 2020_12_06_163805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_12_06_155411) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "team_id"
+    t.string "group_number"
   end
 
   create_table "matches", force: :cascade do |t|
