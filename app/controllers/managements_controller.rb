c74e38ae39673ef386096b7be387179d76d387be
yuchel-246 <yuya.f1117@icloud.com>
2020-12-13 00:26:15 +0900

    得失点

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -1,23 +1,29 @@
 class ManagementsController < ApplicationController
+  
   def result
     @groups = Group.all
     @teams = Team.all
+    @results = Result.all
   end
+
   def schedule
     @teams = Team.all
     @results = Result.all
     @referee_headquarters = "本部"
     @referee_lose_team = "負け審"
-    
   end
+
   def ranking
   end
+
   def show
     @result = Result.find(params[:id])
   end
+
   def edit
     @result = Result.find(params[:id])
   end
+
   def update
     @result = Result.find(params[:id])
     @result.update(params.require(:result).permit(:home_team_score, :away_team_score))
